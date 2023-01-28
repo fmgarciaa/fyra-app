0f70a696502cb62f21cb7e636df833d46a368984
Franklin Garcia <fmanuelgarcia@icloud.com>
2023-01-28 16:28:59 -0500

    [First Commit] Codebase

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+    <h1>Hi Fyra App!</h1>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+			'@components': path.resolve(__dirname, 'src/components/')
+		}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
